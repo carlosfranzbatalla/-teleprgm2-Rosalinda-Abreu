bc0ba52f1739c7f3fd8d1e0720db737dbce35cfb
Rosalinda <abreuparrarosalinda@gmail.com>
2021-04-20 14:53:56 -0400

    first commit

---
+++ Asignación/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Asignación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Progr2!");
+        }
+    }
+}
